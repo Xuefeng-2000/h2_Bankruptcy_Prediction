63dbfb42acd861e5c18e834e0f4ef44553d80dae
xuefeng <1332667077@qq.com>
2021-12-19 10:53:06 +0800

    [modify] main.py

--- src/Decision_Treelib.py
+++ src/Decision_Treelib.py
@@ -1,7 +1,12 @@
+
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import roc_auc_score
+from sklearn.datasets import load_iris
 from sklearn import tree
+import pydotplus
+import os
+from matplotlib import pyplot as plt
 for year in range(1, 6):
     enroll = f'../data_split/enroll_{year}year.csv'
     test = f'../data_split/test_{year}year.csv'
@@ -27,6 +32,17 @@ for year in range(1, 6):
 
     model = tree.DecisionTreeClassifier(criterion='entropy',splitter='best',max_depth=64)
     model.fit(X, y)
+    fig = plt.figure(figsize=(25, 20))
+    _ = tree.plot_tree(
+        model,
+        feature_names=iris.feature_names,
+        class_names=iris.target_names,
+        filled=True
+    )
+
+    # Save picture
+    fig.savefig("decistion_tree.png")
+
 
     right_num = 0
     total = 0
@@ -53,6 +69,8 @@ for year in range(1, 6):
             if ans[0] == label:
                 right_num += 1
             total += 1
+
+
 
     print(f"{year}year:")
     print("acc:", right_num / total)
